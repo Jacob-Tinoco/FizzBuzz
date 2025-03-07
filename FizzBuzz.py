d7316c5ecebb73e2d3b02af75639d5f171aa438e
Jacob Tinoco <158215329+Jacob-Tinoco@users.noreply.github.com>
2025-03-06 18:23:35 -0600

    Carga de Script

---
+++ FizzBuzz.py
@@ -0,0 +1,19 @@
+valores = [15]
+
+def FizzBuzz(valores):
+ resultado = []
+ for i in valores:
+     if i % 3 == 0 and i % 5 == 0:
+         print("FizzBuzz")
+     elif i % 5 == 0:
+         print("Buzz")
+     elif i % 3 == 0:
+         print("Fizz")
+     else: 
+         return(i)
+     return resultado
+resultado = FizzBuzz(valores)
+
+print(f'Valores FizzBuzz: {resultado}')
+
+  
